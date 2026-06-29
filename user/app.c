b7e82a1f8f2d49131f129f2bf9c0cc8a4498f228
Guilty <981196023@qq.com>
2026-06-29 11:29:53 +0800

    事件框架应用

--- user/app.c
+++ user/app.c
@@ -7,39 +7,103 @@
 #include "string.h"
 #include "elog.h"
 #include "bsp.h"
+#include "key.h"
+#include "dht11.h"
+#include "main.h"
 
-void app_dlgcallback(VWM_MESSAGE* pMsg);
-typedef struct app_t{
+void app_dlgcallback(VWM_MESSAGE *pMsg);
+
+void key_change_callback(key_action_t pressorrelase, uint8_t keyvalue);
+
+typedef struct app_t {
     VWM_HWIN main_hwnd;
     VWM_HTIMER blink_timer;
-}app_t;
+    VWM_HTIMER keyscan_timer;
+    VWM_HTIMER sensor_timer;
+    volatile uint8_t temp;
+    volatile uint8_t humi;
+} app_t;
 
 app_t g_app;
 
-void app_init(){
-    memset(&g_app,0,sizeof(app_t));
-    g_app.main_hwnd=MsgExec_CreateModule(app_dlgcallback, 0);
-    if(g_app.main_hwnd==0){
+void app_init() {
+    memset(&g_app, 0, sizeof(app_t));
+    g_app.main_hwnd = MsgExec_CreateModule(app_dlgcallback, 0);
+    if (g_app.main_hwnd == 0) {
         log_e("app_init MsgExec_CreateMoudle error!");
-    }else{
+    } else {
         log_i("app_init MsgExec_CreateMoudle ok!");
     }
+    key_init(key_change_callback);
 }
-void app_dlgcallback(VWM_MESSAGE* pMsg){
+
+void app_dlgcallback(VWM_MESSAGE *pMsg) {
+    uint16_t ledshowdelay;
     switch (pMsg->MsgId) {
         case VWM_INIT_DIALOG:
-            g_app.blink_timer= VWM_CreateTimer(pMsg->hWin,0,500,1);
-            if(g_app.blink_timer==0){
+            g_app.blink_timer = VWM_CreateTimer(pMsg->hWin, 0, 500, 1);
+            if (g_app.blink_timer == 0) {
                 log_e("app_init blink_timer error!");
-            }else{
+            } else {
                 log_i("app_init blink_timer ok!");
                 VWM_StartTimer(g_app.blink_timer);
             }
+            g_app.keyscan_timer = VWM_CreateTimer(pMsg->hWin, 0, 10, 1);
+            if (g_app.keyscan_timer == 0) {
+                log_e("app_init keyscan_timer error!");
+            } else {
+                log_i("app_init keyscan_timer ok!");
+                VWM_StartTimer(g_app.keyscan_timer);
+            }
+            g_app.sensor_timer = VWM_CreateTimer(pMsg->hWin, 0, 1000, 1);
+            if (g_app.sensor_timer == 0) {
+                log_e("app_init sensor_timer error!");
+            } else {
+                log_i("app_init sensor_timer ok!");
+                VWM_StartTimer(g_app.sensor_timer);
+            }
             break;
+
         case VWM_TIMER:
-            LED_Toggle();
+            if (pMsg->Data.v == g_app.blink_timer) {
+                if(HAL_GPIO_ReadPin(LED_GPIO_Port,LED_Pin)==GPIO_PIN_SET){
+                    if (g_app.humi >= 90) {
+                        ledshowdelay = 100;
+                    } else if (g_app.humi <= 50) {
+                        ledshowdelay = 500;
+                    } else {
+                        ledshowdelay = (500 - (g_app.humi - 50) * (500 - 100) / (90 - 50));
+                    }
+                    if (ledshowdelay < 0) {
+                        ledshowdelay = 100;
+                    }
+                    VWM_RestartTimer(g_app.blink_timer,ledshowdelay);
+                    log_i("LED_Timer:%d",ledshowdelay);
+                }
+                LED_Toggle();
+            } else if (pMsg->Data.v == g_app.keyscan_timer) {
+                key_scan();
+            } else if (pMsg->Data.v == g_app.sensor_timer) {
+                if (DHT11_Read_Data(&g_app.temp, &g_app.humi) != 0) {
+                    log_e("DHT11_Read Error!");
+                } else {
+                    log_i("Temp:%d,Humi:%d", g_app.temp, g_app.humi);
+                }
+            }
             break;
         default:
             break;
     }
 }
+
+void key_change_callback(key_action_t pressorrelase, uint8_t keyvalue) {
+    if (pressorrelase == KEY_RELEASEED) {
+        log_i("app_key_release_callback %d", keyvalue);
+        if (keyvalue == KEY_1) {
+            log_i("Temp:%d,Humi:%d", g_app.temp, g_app.humi);
+        }
+    } else if (pressorrelase == KEY_PRESSED) {
+
+    }
+}
+
---
+++ user/key.c
@@ -0,0 +1,76 @@
+#include "key.h"
+#include <stdint.h>
+#include "stm32f1xx_hal.h"
+#include <string.h>
+#include "main.h"
+
+#define KEY_COUNT 3
+#define KEY_QUDOU_MAXCOUNT 3
+
+typedef struct key_t {
+    GPIO_PinState     key_state; //按键的稳定状态 GPIO_PIN_RESET GPIO_PIN_SET
+    uint8_t           key_press_count; //按键状态计数
+    GPIO_TypeDef *    gpio_port;
+    uint16_t          gpio_Pin;
+    uint8_t           key_value;
+}key_t;
+
+typedef struct key_scan_t {
+    key_t                         key[KEY_COUNT];
+    key_change_callback_function_t key_scan_callback;
+}key_scan_t;
+
+key_scan_t g_keyscan;
+
+void key_init(key_change_callback_function_t kayscanfun)
+{
+    memset(&g_keyscan, 0, sizeof(key_scan_t));
+
+    g_keyscan.key[0].gpio_port = KEY1_GPIO_Port;
+    g_keyscan.key[0].gpio_Pin = KEY1_Pin;
+    g_keyscan.key[0].key_value = KEY_1;
+    g_keyscan.key[0].key_state = HAL_GPIO_ReadPin(g_keyscan.key[0].gpio_port, g_keyscan.key[0].gpio_Pin);
+
+    g_keyscan.key[1].gpio_port = KEY2_GPIO_Port;
+    g_keyscan.key[1].gpio_Pin = KEY2_Pin;
+    g_keyscan.key[1].key_value = KEY_2;
+    g_keyscan.key[1].key_state = HAL_GPIO_ReadPin(g_keyscan.key[1].gpio_port, g_keyscan.key[1].gpio_Pin);
+
+//    g_keyscan.key[2].gpio_port = KEY3_GPIO_Port;
+//    g_keyscan.key[2].gpio_Pin = KEY3_Pin;
+//    g_keyscan.key[2].key_value = KEY_3;
+//    g_keyscan.key[2].key_state = HAL_GPIO_ReadPin(g_keyscan.key[2].gpio_port, g_keyscan.key[2].gpio_Pin);
+
+    g_keyscan.key_scan_callback = kayscanfun;
+}
+
+void key_scan(void)
+{
+    uint8_t i;
+    GPIO_PinState nowstate;
+
+    for (i=0; i<KEY_COUNT; i++) {
+        nowstate = HAL_GPIO_ReadPin(g_keyscan.key[i].gpio_port, g_keyscan.key[i].gpio_Pin);
+
+        if (g_keyscan.key[i].key_state == nowstate) {
+            g_keyscan.key[i].key_press_count = 0;
+        } else {
+            g_keyscan.key[i].key_press_count++;
+            if (g_keyscan.key[i].key_press_count >= KEY_QUDOU_MAXCOUNT) {
+                g_keyscan.key[i].key_state = nowstate;
+
+                if (nowstate == GPIO_PIN_RESET) {
+                    //被按下
+                    if (g_keyscan.key_scan_callback != NULL) {
+                        g_keyscan.key_scan_callback(KEY_PRESSED, g_keyscan.key[i].key_value);
+                    }
+                } else {
+                    //松开
+                    if (g_keyscan.key_scan_callback != NULL) {
+                        g_keyscan.key_scan_callback(KEY_RELEASEED, g_keyscan.key[i].key_value);
+                    }
+                }
+            }
+        }
+    }
+}
