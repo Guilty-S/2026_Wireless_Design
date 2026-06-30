f68a6232990139407d6517cd9f3e75c903606a2f
Guilty <981196023@qq.com>
2026-06-30 13:41:21 +0800

    添加虚拟屏设计文件

---
+++ wireless_page/design/output/DefineID.h
@@ -0,0 +1,15 @@
+//创建画面主界面的命令索引ID
+#define  _SCREEN_主界面                                                           0
+
+#define  _BTN_主界面_BUTTON1                                                      1
+
+#define  _TXT_DIS__主界面_TEXT_DISPLAY1                                           2
+
+#define  _TXT_DIS__主界面_TEXT_DISPLAY2                                           3
+
+#define  _TXT_DIS__主界面_TEXT_DISPLAY3                                           4
+
+#define  _TXT_DIS__主界面_TEXT_DISPLAY4                                           5
+
+#define  _TXT_DIS__主界面_TEXT_DISPLAY5                                           6
+
